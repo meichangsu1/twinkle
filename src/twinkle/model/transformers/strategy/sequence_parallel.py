991a17d8c86b0e09a392eb31ceb3edc38f32ef5b
meichangsu1 <1484603386@qq.com>
2026-03-23 09:31:38 +0800

    wip

--- src/twinkle/model/transformers/strategy/sequence_parallel.py
+++ src/twinkle/model/transformers/strategy/sequence_parallel.py
@@ -340,14 +340,17 @@ class DistributedAttention(torch.nn.Module):
         else:
             query_layer, key_layer, value_layer = query, key, value
 
-        position_ids = kwargs.pop('position_ids')
+        position_ids = kwargs.pop('position_ids', None)
         if position_ids is not None:
-            shape0 = position_ids.shape[0]
-            position_ids_output = torch.empty((shape0 * self.sequence_parallel.sp_world_size, position_ids.shape[1]),
-                                              dtype=position_ids.dtype,
-                                              device=position_ids.device)
+            position_ids = position_ids.contiguous()
+            gathered_shape = (self.sequence_parallel.sp_world_size, *position_ids.shape)
+            position_ids_output = torch.empty(
+                gathered_shape,
+                dtype=position_ids.dtype,
+                device=position_ids.device,
+            )
             dist.all_gather_into_tensor(position_ids_output, position_ids, group=self.sequence_parallel._sp_group)
-            position_ids = torch.cat(position_ids_output.split(shape0, dim=0), dim=1)
+            position_ids = torch.cat(tuple(position_ids_output.unbind(dim=0)), dim=-1).contiguous()
 
         context_layer = self.local_attn(
             query_layer, key_layer, value_layer, attention_mask, *args, position_ids=position_ids, **kwargs)
